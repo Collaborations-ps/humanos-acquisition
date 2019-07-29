f7b9c5bfd095fcad48751eb262e79297c51193a6
George Viazemsky <george@startupcraft.io>
2019-07-29 22:44:35 +0300

    Remove unused code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const Koa = require('koa')
 const Next = require('next')
-// const Router = require('koa-router')
-// const ImapClient = require('emailjs-imap-client').default
-// const mimelib = require('mimelib-noiconv')
 
 const router = require('./routes')
 
@@ -13,66 +10,6 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = new Koa()
-
-  /* router.get('/imap', async ctx => {
-    console.log(ctx)
-    const client = new ImapClient('imap.gmail.com', 993, {
-      useSecureTransport: true,
-      requireTLS: true,
-      auth: {
-        user: 'msnake.dubna@gmail.com',
-        xoauth2:
-          'ya29.GltUB4m1A1SGfcSowApHPYCfx9oUVshMBnZ2BS8FwDMfnMSrJZSv2XHa3QZZdGOyd_UA5rdz9oLNrw5owec0OUkRsvHdYtrVpLzb3yAe2R4fHodfG-bLtZYxzbgD',
-      },
-    })
-
-    client.onerror = error => console.log(error.message)
-
-    await client.connect()
-
-    console.log(await client.listMailboxes())
-
-    const messages = await client.listMessages('INBOX', '1:10', [
-      '(UID X-GM-THRID BODY.PEEK[HEADER.FIELDS (FROM TO CC Date)])',
-    ])
-
-    console.log(messages)
-
-    messages.reduce(async (acc, message) => {
-      const params = message['body[header.fields (from to cc date)]'].split(
-        '\r\n',
-      )
-
-      const parsed = {
-        from: '',
-        to: '',
-        date: '',
-        threadId: message['x-gm-thrid'],
-        cc: '',
-      }
-
-      params.forEach(param => {
-        if (param.startsWith('From: ')) {
-          parsed.from = mimelib.parseAddresses(param.replace('From: ', ''))
-        }
-        if (param.startsWith('To: ')) {
-          parsed.to = mimelib.parseAddresses(param.replace('To: ', ''))
-        }
-        if (param.startsWith('Cc: ')) {
-          parsed.cc = mimelib.parseAddresses(param.replace('Cc: ', ''))
-        }
-        if (param.startsWith('Date: ')) {
-          parsed.date = param.replace('Date: ', '')
-        }
-      })
-
-      console.log(parsed)
-
-      return acc
-    }, new Map())
-
-    client.close()
-  }) */
 
   router.get('*', async ctx => {
     await handle(ctx.req, ctx.res)
